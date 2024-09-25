01c49b7e831c84c017f01dd63203451454b0477d
JirkaDellOro <del@hs-furtwangen.de>
2024-09-25 13:08:48 +0200

    started repo without videos

---
+++ Theory2.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1721116281759" ID="ID_1337734116" MODIFIED="1721116287109" TEXT="Code2"/>
+</map>
