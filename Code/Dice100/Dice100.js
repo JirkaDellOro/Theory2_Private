505d6d51bd7d8fc10dc644012c099277763f1781
JirkaDellOro <del@hs-furtwangen.de>
2024-11-21 10:41:46 +0100

    play fair, player 1 has final rolls

--- Code/Dice100/Dice100.js
+++ Code/Dice100/Dice100.js
@@ -3,7 +3,7 @@ let score = [0, 0];
 let active = 0;
 let potential = 0;
 let strategy = [strategy10, strategy10];
-// strategy[1] = Reflect.get(window, "HundoDice").strategy
+strategy[1] = Reflect.get(window, "HundoDice").strategy;
 do {
     let go = strategy[active](score, active, potential);
     if (go) {
@@ -15,14 +15,17 @@ do {
     }
     else {
         score[active] += potential;
-        if (score[active] >= 100)
+        if (active == 1 && (score[active] >= 100 || score[1 - active] >= 100))
             break;
         togglePlayer();
     }
 } while (true);
+togglePlayer();
+if (score[active] < score[1 - active])
+    togglePlayer();
 console.log(`Player ${active} won with ${score[active]} points`);
 function strategy10(_score, _active, _potential) {
-    let go = _potential < 10;
+    let go = _potential < 50;
     return go;
 }
 function togglePlayer() {
--- Code/Dice100/Dice100.ts
+++ Code/Dice100/Dice100.ts
@@ -3,7 +3,7 @@ let active: number = 0
 let potential: number = 0
 let strategy: Function[] = [strategy10, strategy10]
 
-// strategy[1] = Reflect.get(window, "HundoDice").strategy
+strategy[1] = Reflect.get(window, "HundoDice").strategy
 
 do {
   let go: boolean = strategy[active](score, active, potential)
@@ -15,16 +15,19 @@ do {
       togglePlayer()
   } else {
     score[active] += potential
-    if (score[active] >= 100)
+    if (active == 1 && (score[active] >= 100 || score[1 - active] >= 100))
       break
     togglePlayer()
   }
 } while (true)
 
+togglePlayer()
+if (score[active] < score[1 - active])
+  togglePlayer()
 console.log(`Player ${active} won with ${score[active]} points`)
 
 function strategy10(_score: number[], _active: number, _potential: number): boolean {
-  let go: boolean = _potential < 10
+  let go: boolean = _potential < 50
   return go
 }
 
