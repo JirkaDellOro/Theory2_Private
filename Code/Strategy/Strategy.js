b7a48728c2dc6423a586ed1c735d55a6587bff25
JirkaDellOro <del@hs-furtwangen.de>
2024-12-16 17:20:44 +0100

    strategy go when potential < 20

--- Code/Strategy/Strategy.js
+++ Code/Strategy/Strategy.js
@@ -2,7 +2,7 @@ export var Jirka;
 (function (Jirka) {
     function strategy(_score, _active, _potential) {
         // return Math.random() < 0.5;
-        return _score[_active] < 20;
+        return _potential < 20;
     }
     Jirka.strategy = strategy;
 })(Jirka || (Jirka = {}));
--- Code/Strategy/Strategy.ts
+++ Code/Strategy/Strategy.ts
@@ -1,6 +1,6 @@
 export namespace Jirka {
   export function strategy(_score: number[], _active: number, _potential: number): boolean {
     // return Math.random() < 0.5;
-    return _score[_active] < 20;
+    return _potential < 20;
   }
 }
